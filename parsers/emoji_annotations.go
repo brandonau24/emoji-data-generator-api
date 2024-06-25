f89b7a587653d0cc087321906f1c31aa191659ca
bau <tech.brandonau@gmail.com>
2024-06-25 16:35:49 -0400

    fix: keep codepoints as hexadecimal codepoints string in annoations map instead of automatically being converted into an emoji

--- parsers/emoji_annotations.go
+++ parsers/emoji_annotations.go
@@ -15,7 +15,15 @@ type Annotation struct {
 	Default []string
 }
 
-func ParseAnnotations(annotations string) map[string][]string {
+type EmojiHexadecimalCodepoint string
+
+func (codepoint EmojiHexadecimalCodepoint) MarshalJSON() ([]byte, error) {
+	codepointStr := fmt.Sprintf("\"%s\"", string(codepoint))
+
+	return json.Marshal(codepointStr)
+}
+
+func ParseAnnotations(annotations string) map[EmojiHexadecimalCodepoint][]string {
 	var annotationsFileMap AnnotationsFile
 	err := json.Unmarshal([]byte(annotations), &annotationsFileMap)
 
@@ -23,14 +31,14 @@ func ParseAnnotations(annotations string) map[string][]string {
 		panic(err)
 	}
 
-	parsedAnnotations := make(map[string][]string, 0)
+	parsedAnnotations := make(map[EmojiHexadecimalCodepoint][]string, 0)
 
 	nestedAnnotations := annotationsFileMap.Annotations.Annotations
 
 	for emoji, emojiAnnotations := range nestedAnnotations {
 		emojiCodepoints := []rune(emoji)
 		emojiCodepointsHexadecimal := fmt.Sprintf("%X", emojiCodepoints)
-		parsedAnnotations[emojiCodepointsHexadecimal] = emojiAnnotations.Default
+		parsedAnnotations[EmojiHexadecimalCodepoint(emojiCodepointsHexadecimal)] = emojiAnnotations.Default
 	}
 
 	return parsedAnnotations
