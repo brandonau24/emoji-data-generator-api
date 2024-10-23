3d6fdbce12d6fe5e0d30bc6dd799d7650fc210b2
bau <tech.brandonau@gmail.com>
2024-10-23 03:22:40 +0000

    test: refactor emoji_annotations_test.go to use table-driven tests

--- cmd/api_server/internal/parsers/emoji_annotations_test.go
+++ cmd/api_server/internal/parsers/emoji_annotations_test.go
@@ -10,68 +10,66 @@ import (
 	test_helpers "github.com/brandonau24/emoji-data-generator-api/cmd/api_server/internal/internal"
 )
 
-func Test_ParseAnnotations(t *testing.T) {
-	mockHttpServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == test_helpers.MOCK_UNICODE_ANNOTATIONS_PATH {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(`
-{
-	"annotations": {
-		"annotations": {
-			"😀": {
-				"default": [
-					"face",
-					"grin",
-					"grinning face"
-				],
-				"tts": [
-					"grinning face"
-				]
+type emojiAnnotationsTest struct {
+	Name                string
+	StatusCode          int
+	ResponseBody        []byte
+	ExpectedAnnotations map[string]Annotation
+}
+
+func TestParseAnnotations(t *testing.T) {
+	tests := map[string]emojiAnnotationsTest{
+		"maps emoji to its annotations": {
+			StatusCode: http.StatusOK,
+			ResponseBody: []byte(`
+			{
+				"annotations": {
+					"annotations": {
+						"😀": {
+							"default": [
+								"face",
+								"grin",
+								"grinning face"
+							],
+							"tts": [
+								"grinning face"
+							]
+						}
+					}
+				}
+			}`),
+			ExpectedAnnotations: map[string]Annotation{
+				"😀": {
+					Tts:     []string{"grinning face"},
+					Default: []string{"face", "grin", "grinning face"},
+				},
+			},
+		},
+		"nil is returned when request fails": {
+			StatusCode:          http.StatusBadRequest,
+			ResponseBody:        []byte(""),
+			ExpectedAnnotations: nil,
+		},
+	}
+
+	for name, testcase := range tests {
+		t.Run(name, func(t *testing.T) {
+			mockHttpServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path == test_helpers.MOCK_UNICODE_ANNOTATIONS_PATH {
+					w.WriteHeader(testcase.StatusCode)
+					w.Write(testcase.ResponseBody)
+				}
+			}))
+
+			defer mockHttpServer.Close()
+
+			annotations := ParseAnnotations(test_helpers.MockDataUrlProvider{
+				BaseUrl: mockHttpServer.URL,
+			})
+
+			if !reflect.DeepEqual(annotations, testcase.ExpectedAnnotations) {
+				t.Errorf("%v: expected: %v, got: %v", testcase.Name, testcase.ExpectedAnnotations, annotations)
 			}
-		}
-	}
-}
-`))
-		}
-	}))
-
-	defer mockHttpServer.Close()
-
-	annotations := ParseAnnotations(test_helpers.MockDataUrlProvider{
-		BaseUrl: mockHttpServer.URL,
-	})
-
-	expectedAnnotations := []string{"face", "grin", "grinning face"}
-
-	emojiAnnotations, ok := annotations["😀"]
-	if !ok {
-		t.Errorf("Failed to find annotations for 😀")
-	}
-
-	if !reflect.DeepEqual(emojiAnnotations.Default, expectedAnnotations) {
-		t.Errorf("Failed to map annotations. Received %v, expected %v", emojiAnnotations, expectedAnnotations)
-	}
-
-	if emojiAnnotations.Tts[0] != "grinning face" {
-		t.Errorf("Failed to map tts. Received %v, expected \"grinning face\"", emojiAnnotations.Tts)
-	}
-}
-
-func Test_ParseAnnotations_ReturnsNil_WhenRequestFails(t *testing.T) {
-	mockHttpServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == test_helpers.MOCK_UNICODE_ANNOTATIONS_PATH {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(""))
-		}
-	}))
-
-	defer mockHttpServer.Close()
-
-	annotations := ParseAnnotations(test_helpers.MockDataUrlProvider{
-		BaseUrl: mockHttpServer.URL,
-	})
-
-	if annotations != nil {
-		t.Errorf("Expected no annotations returned when annotations request fails. Received %v", annotations)
+		})
 	}
 }
