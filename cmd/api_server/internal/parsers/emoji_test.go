4f644d5f3680dab4677015a1b9a1aa7cce1bb663
bau <tech.brandonau@gmail.com>
2024-10-23 03:43:51 +0000

    test: refactor emoji_test.go to use table-driven tests

--- cmd/api_server/internal/parsers/emoji_test.go
+++ cmd/api_server/internal/parsers/emoji_test.go
@@ -4,42 +4,80 @@ import (
 	"testing"
 )
 
-func Test_ParseCodepoint(t *testing.T) {
-	emojiFields := []string{"1F600", ";", "fully-qualified", "#", "😀", "E1.0", "grinning face"}
+func TestParseCodepoints(t *testing.T) {
+	tests := map[string]struct {
+		expectedCodepoints string
+		emojiFields        []string
+	}{
+		"parses a single codepoint": {
+			expectedCodepoints: "1F600",
+			emojiFields:        []string{"1F600", ";", "fully-qualified", "#", "😀", "E1.0", "grinning", "face"},
+		},
+		"parses multiple codepoints": {
+			expectedCodepoints: "1F636 200D 1F32B FE0F",
+			emojiFields:        []string{"1F636", "200D", "1F32B", "FE0F", ";", "fully-qualified", "#", "😶‍🌫️", "E13.1", "face", "in", "clouds"},
+		},
+	}
 
-	codepoints := ParseCodepoints(emojiFields)
+	for name, testcase := range tests {
+		t.Run(name, func(t *testing.T) {
+			codepoints := ParseCodepoints(testcase.emojiFields)
 
-	if codepoints != "1F600" {
-		t.Errorf("Expected 1F600 codepoint, received %v", codepoints)
+			if codepoints != testcase.expectedCodepoints {
+				t.Errorf("%v: expected: %v, got: %v", name, testcase.expectedCodepoints, codepoints)
+			}
+		})
 	}
 }
 
-func Test_ParseCodepoints(t *testing.T) {
-	emojiFields := []string{"1F636", "200D", "1F32B", "FE0F", ";", "fully-qualified", "#", "😶‍🌫️", "E13.1", "face", "in", "clouds"}
+func TestParseEmojiName(t *testing.T) {
+	tests := map[string]struct {
+		expectedName string
+		emojiFields  []string
+	}{
+		"parses emoji name containing one word": {
+			expectedName: "ogre",
+			emojiFields:  []string{"1F479", ";", "fully-qualified", "#", "👹", "E0.6", "ogre"},
+		},
+		"parses emoji name containing multiple words": {
+			expectedName: "skull and crossbones",
+			emojiFields:  []string{"2620", "FE0F", ";", "fully-qualified", "#", "☠️", "E1.0", "skull", "and", "crossbones"},
+		},
+	}
 
-	codepoints := ParseCodepoints(emojiFields)
+	for name, testcase := range tests {
+		t.Run(name, func(t *testing.T) {
+			emojiName := ParseEmojiName(testcase.emojiFields)
 
-	if codepoints != "1F636 200D 1F32B FE0F" {
-		t.Errorf("Expected 1F636 200D 1F32B FE0F codepoints, received %v", codepoints)
+			if emojiName != testcase.expectedName {
+				t.Errorf("%v: expected: %v, got: %v", name, testcase.expectedName, emojiName)
+			}
+		})
 	}
 }
 
-func Test_ParseEmojiName(t *testing.T) {
-	emojiFields := []string{"1F600", ";", "fully-qualified", "#", "😀", "E1.0", "grinning face"}
-
-	name := ParseEmojiName(emojiFields)
-
-	if name != "grinning face" {
-		t.Errorf("Expected \"grinning face\", received %v", name)
+func TestParseEmojiCharacter(t *testing.T) {
+	tests := map[string]struct {
+		expectedEmoji string
+		emojiFields   []string
+	}{
+		"parses emoji character": {
+			expectedEmoji: "😀",
+			emojiFields:   []string{"1F600", ";", "fully-qualified", "#", "😀", "E1.0", "grinning face"},
+		},
+		"parses multicomponent emoji character": {
+			expectedEmoji: "⛓️‍💥",
+			emojiFields:   []string{"26D3", "FE0F", "200D", "1F4A5", "fully-qualified", "#", "⛓️‍💥", "E15.1", "broken", "chain"}, // Broken chain is actually ⛓️‍ + 💥
+		},
 	}
-}
 
-func Test_ParseEmojiCharacter(t *testing.T) {
-	emojiFields := []string{"1F600", ";", "fully-qualified", "#", "😀", "E1.0", "grinning face"}
+	for name, testcase := range tests {
+		t.Run(name, func(t *testing.T) {
+			emoji := ParseEmojiCharacter(testcase.emojiFields)
 
-	character := ParseEmojiCharacter(emojiFields)
-
-	if character != "😀" {
-		t.Errorf("Expected \"😀\", received %v", character)
+			if emoji != testcase.expectedEmoji {
+				t.Errorf("%v: expected: %v, got: %v", name, testcase.expectedEmoji, emoji)
+			}
+		})
 	}
 }
